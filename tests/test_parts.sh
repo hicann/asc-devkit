21c69e0595f37e1f6f6180c274706a30204277bc
zhangfeifan0709 <zhangfeifan7@huawei.com>
2026-09-14 09:06:46 +0800

    C_API支持9201UT
    Co-authored-by: zhangfeifan0709<zhangfeifan7@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !5733 merge 9201_ut into master
    
    C_API支持9201UT
    
    Created-by: zhangfeifan0709
    Commit-by: zhangfeifan0709
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    支持9201UT
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #xxx-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] 🐛 fix: Bug 修复
    - [x] ✨ feat: 新功能
    - [ ] ⚡ perf: 性能优化
    - [ ] ♻️ refactor: 代码重构
    - [ ] 🧪 test: 新增或修改测试
    - [ ] 📝 docs: 文档更新
    - [ ] 🔧 ci: CI/CD 配置修改
    - [ ] ↩️ revert: 回退
    - [ ] 🧹 chore: 其他，请具体描述
    
    
    See merge request: cann/asc-devkit!5733

--- tests/test_parts.sh
+++ tests/test_parts.sh
@@ -114,6 +114,8 @@ basic_test_five_targets=(
     ascendc_ut_c_api_ascend910B1_AIV
     ascendc_ut_c_api_ascend950pr_9599_AIC
     ascendc_ut_c_api_ascend950pr_9599_AIV
+    ascendc_ut_c_api_ascend960dt_AIC
+    ascendc_ut_c_api_ascend960dt_AIV
     ascendc_ut_reg_compute_ascend950pr_9599
     ascendc_ut_tensor_api_NPU_ARCH_3510_AIC
     ascendc_ut_tensor_api_NPU_ARCH_3510_AIV
