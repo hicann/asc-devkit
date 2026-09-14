2a2417877a78a90290e2f39ac52b69cac6b62c4d
li-yuanjie-da <liyuanjie5@h-partners.com>
2026-09-14 19:38:05 +0800

    revert print
    Co-authored-by: li-yuanjie-da<liyuanjie5@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !6000 merge feature/tensor_api_from_9.0.0 into feature/tensor_api_from_9.0.0
    
    revert print
    
    Created-by: li-yuanjie-da
    Commit-by: li-yuanjie-da
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    回退print接口
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #xxx-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] 🐛 fix: Bug 修复
    - [ ] ✨ feat: 新功能
    - [ ] ⚡ perf: 性能优化
    - [ ] ♻️ refactor: 代码重构
    - [ ] 🧪 test: 新增或修改测试
    - [ ] 📝 docs: 文档更新
    - [ ] 🔧 ci: CI/CD 配置修改
    - [ ] ↩️ revert: 回退
    - [ ] 🧹 chore: 其他，请具体描述
    
    
    See merge request: cann/asc-devkit!6000

--- include/tensor_api/tensor.h
+++ include/tensor_api/tensor.h
@@ -24,7 +24,6 @@
 #include "tensor_api/tensor/layout_interface.h"
 #include "tensor_api/tensor/pointer.h"
 #include "tensor_api/utils/utils.h"
-#include "tensor_api/utils/print.h"
 #include "tensor_api/arch/arch.h"
 #include "tensor_api/atom/atom.h"
 #include "tensor_api/algorithm/copy.h"
